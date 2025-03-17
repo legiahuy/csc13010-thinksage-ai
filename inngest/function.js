97295dde40428228dfcbed351deb660f3cfe97c2
Kh13m <vngkhiem22@clc.fitus.edu.vn>
2025-03-17 15:33:51 +0700

    change api inngest

--- inngest/function.js
+++ inngest/function.js
@@ -9,12 +9,40 @@ export const helloWorld = inngest.createFunction(
   }
 );
 
-/*
+const BASE_URL='https://aigurulab.tech';
 export const GenerateVideoData = inngest.createFunction(
   { id: "generate-video-data" },
   { event: "generate-video-data" },
   async ({ event, step }) => {
     
-  }
+    const {script, topic, voice, videoStyle, caption, title} = event?.data;
+    //generate audio
+    const GenerateAudioFile = await step.run(
+        "generateAudioFile",
+        async()=>{
+            const result = await axios.post(BASE_URL+'/api/text-to-speech',
+                {
+                    input: script,
+                    voice: voice
+                },
+                {
+                    headers: {
+                        'x-api-key': process.env.NEXT_PUBLIC_AIGURULAB_API_KEY, // Your API Key
+                        'Content-Type': 'application/json', // Content Type
+                    },
+                })
+             console.log(result.data.audio) //Output Result: Audio Mp3 Url
+            return result.data.audio;
+        }
+    )
+    //generate captions
+
+    //generate image prompt
+
+    //generate image
+
+    //Save all to db
+    return GenerateAudioFile
+    }
 );
-*/
+
